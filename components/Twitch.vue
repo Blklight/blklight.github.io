4d3ec76f91807cef3035066289fed81b1544ed3f
Julian Silva da Cunha <juliancunha2010@hotmail.com>
2021-01-23 18:23:45 -0300

    Twitch test

--- components/Twitch.vue
+++ components/Twitch.vue
@@ -5,12 +5,24 @@
     width="100%"
   >
   </iframe> -->
-  <iframe
-    src="https://player.twitch.tv/?channel=teamblklight&parent=blklight.github.io&muted=true&layout=video-with-chat"
-    height="600"
-    width="100%"
-    muted="true"
-    allowfullscreen="true"
-  >
-  </iframe>
+  <div class="row">
+    <div class="col-md-8 col-12">
+      <iframe
+        src="https://player.twitch.tv/?channel=teamblklight&parent=blklight.github.io&muted=true&layout=video-with-chat"
+        height="600"
+        width="100%"
+        muted="true"
+        allowfullscreen="true"
+      >
+      </iframe>
+    </div>
+    <div class="col-md-4 col-12">
+      <iframe
+        src="https://www.twitch.tv/embed/teamblklight/chat?parent=blklight.github.io"
+        height="600"
+        width="100%"
+      >
+      </iframe>
+    </div>
+  </div>
 </template>
