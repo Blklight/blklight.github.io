cd89032cefb4aee72c2ab084cdb5bb785d94c31c
Julian Silva da Cunha <juliancunha2010@hotmail.com>
2021-01-28 16:34:23 -0300

    New article and small changes

--- pages/template-email.vue
+++ pages/template-email.vue
@@ -1,17 +1,7 @@
 <template>
   <div>
     <header class="email-header">
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        class="email-header-logo"
-        width="110"
-        height="20"
-      >
-        <path
-          d="M100.414 20a1 1 0 01-.706-1.707l6.543-6.542a1.768 1.768 0 00-2.5-2.5L93.586 19.414a2 2 0 01-1.414.586H91.5a1.5 1.5 0 01-1.5-1.5V2a2 2 0 012-2h7.586a1 1 0 01.707 1.707L93.75 8.25a1.767 1.767 0 002.5 2.5L106.414.586A2 2 0 01107.828 0h.672a1.5 1.5 0 011.5 1.5V18a2 2 0 01-2 2zM69.5 20a2 2 0 01-2-2V2a2 2 0 012-2h16a2 2 0 012 2v16a2 2 0 01-2 2zm2.25-15.75a1.77 1.77 0 000 2.5l9 9a1.768 1.768 0 102.5-2.5l-9-9a1.769 1.769 0 00-2.5 0zM47 20a2 2 0 01-2-2V2a2 2 0 012-2h16a2 2 0 012 2v13.086a1 1 0 01-1.707.707L58 10.5a1.768 1.768 0 10-2.5 2.5l5.294 5.293A1 1 0 0160.086 20zm4.25-16.25l-2.5 2.5a1.768 1.768 0 002.5 2.5l2.5-2.5a1.768 1.768 0 00-2.5-2.5zM24.5 20a2 2 0 01-2-2V4.914a1 1 0 011.707-.707L28 8a1.768 1.768 0 102.5-2.5l-3.792-3.793A1 1 0 0127.414 0H40.5a2 2 0 012 2v13.086a1 1 0 01-1.707.707L37 12a1.768 1.768 0 00-2.5 2.5l3.793 3.793A1 1 0 0137.586 20zM2 20a2 2 0 01-2-2V2a2 2 0 012-2h13.086a1 1 0 01.707 1.707L4.75 12.75a1.768 1.768 0 002.5 2.5L18.292 4.207A1 1 0 0120 4.914V18a2 2 0 01-2 2z"
-          fill="#fff"
-        />
-      </svg>
+      <img src="../static/chaos.png" class="email-header-logo" alt="" />
     </header>
     <div class="email-container">
       <section class="email-body">
@@ -27,12 +17,6 @@
           <p class="email-box-title">Código de acesso</p>
           <h1 class="email-box-code">123456</h1>
         </div>
-        <!-- <div>
-          <p class="email-body-description">Obrigada,</p>
-          <p class="email-body-description" style="margin-bottom: 0">
-            Time CHAOS
-          </p>
-        </div> -->
       </section>
     </div>
 
@@ -51,13 +35,21 @@
         </div>
       </div>
     </div>
-    <footer style="background-color: #000; padding: 12px">
-      <p style="color: #ffffff; text-align: center">
+    <footer style="background-color: #000; padding: 20px">
+      <p style="color: #ffffff; text-align: center; font-size: 12px">
         <a class="email-cancel-subscription" href="">Cancelar inscrição</a> de
         qualquer comunicação relativa a este convite.
       </p>
-      <p style="color: #ffffff; text-align: center">
-        Feito movido a Música, e com o coração por BOHR
+      <p
+        style="
+          color: #ffffff;
+          text-align: center;
+          font-size: 12px;
+          margin-bottom: 0;
+        "
+      >
+        Feito movido a <img src="../static/song.png" alt="" />, e com o coração
+        <img src="../static/heart.png" alt="" /> por BOHR
       </p>
     </footer>
   </div>
@@ -98,7 +90,7 @@ export default {
   font-style: italic;
   font-weight: 900;
   margin: 0;
-  font-size: 24px;
+  font-size: 32px;
 }
 .email-body-subtitle {
   color: #fff;
@@ -107,12 +99,13 @@ export default {
   font-style: italic;
   font-weight: 900;
   margin: 0;
-  font-size: 24px;
+  font-size: 32px;
 }
 .email-body-description {
   color: #fff;
   margin: 24px 0;
   line-height: 1.5;
+  font-size: 16px;
 }
 .email-box {
   background-color: #1a1a1a;
@@ -139,6 +132,12 @@ export default {
 .email-cancel-subscription {
   color: #fff;
   text-decoration: underline;
+}
+
+.email-cancel-subscription:hover {
+  color: #fff;
+  text-decoration: underline;
+  cursor: pointer;
 }
 
 .linear-background {
