1650457a9d47ec471338aa30f99edc6d7752a053
Julian Silva da Cunha <juliancunha2010@hotmail.com>
2021-10-23 23:32:06 -0300

    e6d473a

---
+++ _nuxt/static/1635042654/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b,c){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1635042654",layout:"default",error:a,state:{isDark:b,isOpen:false},serverRendered:b,routePath:c,config:{_app:{basePath:c,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"c3341e1b"}}}}(null,true,"\u002F"));
