ba088faa0c2d4419355d415b1a601f82ef0e014e
Julian Silva da Cunha <juliancunha2010@hotmail.com>
2023-03-16 20:05:02 -0300

    Add components inside articles

--- components/Mdx.tsx
+++ components/Mdx.tsx
@@ -3,10 +3,22 @@ import dynamic from "next/dynamic";
 import { useMDXComponent } from "next-contentlayer/hooks";
 import TOCInline from "./ArticleRelated/TOCInline";
 import Pre from "./ArticleRelated/Pre";
+import Tags from "@/components/Tags";
+import {
+  Tabs,
+  TabsContent,
+  TabsList,
+  TabsTrigger,
+} from "@/components/ui/tabs-for-articles";
 
 const components = {
   TOCInline,
   pre: Pre,
+  Tags,
+  Tabs,
+  TabsContent,
+  TabsList,
+  TabsTrigger,
   wrapper: ({ components, layout, ...rest }: any) => {
     const Layout = dynamic(() => import(`./ArticleLayouts/${layout}`));
     return <Layout {...rest} />;
